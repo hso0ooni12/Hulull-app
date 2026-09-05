d4885791ab8a97814ab5b1878196697820002b67
hso0ooni12 <102685479+hso0ooni12@users.noreply.github.com>
2026-09-05 17:31:02 +0300

    Enable Hulull 2.0 dashboard and mobile enhancements

--- config.js
+++ config.js
@@ -4,12 +4,14 @@ window.APP_CONFIG = {
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_c5x6A7oaB3TWaTyMVYo_yg_PAwSSJZn'
 };
 
-// تحميل إضافات التقارير والحجز قبل كود التطبيق الرئيسي مع الإبقاء على index.html والطباعة كما هما.
+// تحميل إضافات التقارير والحجز والتطويرات بدون تغيير بيانات السندات الحالية.
 const hulullScript = src => document.write('<script src="'+src+'"></'+'script>');
 document.write('<link rel="stylesheet" href="reports.css">');
+document.write('<link rel="stylesheet" href="hulull-v2.css">');
 hulullScript('https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js');
 hulullScript('analytics.js');
 hulullScript('booking-rules.js');
 hulullScript('free-booking-map.js');
 hulullScript('excel-export.js');
 hulullScript('upgrade-init.js');
+hulullScript('hulull-v2.js');
